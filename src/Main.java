13c1f1ebe20937a2f5f56cea84021c692d3db62f
shravnisingh <shravanisingh22@gmail.com>
2024-12-12 23:48:48 +0530

    UC-02

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        Node firstNode = new Node(56);
-        Node secondNode = new Node(30);
-        Node thirdNode = new Node(70);
-        list.head = firstNode;
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
-        System.out.println("Linked List:");
-        list.display();
+        list.head = new Node(70);
+        list.add(30);
+        list.add(56);
+        list.printList();
     }
 }

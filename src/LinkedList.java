82c48d827014b84015cfbdf5394d96ce4607ce48
shravnisingh <shravanisingh22@gmail.com>
2024-12-12 23:46:51 +0530

    UC-01

---
+++ src/LinkedList.java
@@ -0,0 +1,15 @@
+public class LinkedList {
+    public Node head;
+    public void display() {
+        if (head == null) {
+            System.out.println("Linked List is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("LinkedList");
+        LinkedList list = new LinkedList();
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+        list.head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        System.out.println("Linked List:");
+        list.display();
     }
 }
